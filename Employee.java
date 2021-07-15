f2be3152f8156dfffc9f93653cf3af9b9b5fbf3e
unknown <ibmb1java21@iiht.tech>
2021-07-15 16:44:22 +0530

    Two files

---
+++ Employee.java
@@ -0,0 +1,6 @@
+public class Employee
+{
+private int empid;
+priate double salaey;
+public Employee(){}
+public Employee(int ssn,String name,int age,int empid,double salary)
---
+++ Person.java
@@ -0,0 +1,19 @@
+public class Person
+{
+private int ssn;
+private String name;
+private int age;
+public Person(){}
+public Person(int ssn,String name,int age)
+{
+this.ssn=ssn;
+this.name=name;
+this.age=age;
+}
+public boid showPerson()
+{
+System.out.ptintln("Ssn is "+ssn);
+System.out.ptintln("Name is "+name);
+System.out.ptintln("Age is "+age);
+}
+}
